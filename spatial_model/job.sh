9bed03b0c02c492dc0c5eafb6c0ca5d430e60d2a
TommasoTarchi <tommaso.tarchi@gmail.com>
2023-09-19 17:16:31 +0200

    bug fix

--- spatial_model/job.sh
+++ spatial_model/job.sh
@@ -34,7 +34,7 @@ echo "# " >> "$datafile"
 echo "simulation_time,elapsed_time,state_area0,state_area1,state_area2,state_area3" >> "$datafile"
 for index in {1..10}
 do
-    python3 ../src/.py --m 3 --fitness "static_inc" --output "final_state" --datafile "$datafile"
+    python3 ../src/spatial.py --m 3 --fitness "static_inc" --output "final_state" --datafile "$datafile"
     echo >> "$datafile"
 done 
 
